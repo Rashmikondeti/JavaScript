2abfceb43afeb262b55e5c00bc80e7e789ad56b5
Kondeti Rashmi <rashmikondeti18@gmail.com>
2026-03-04 16:18:37 +0530

    Delete task5.js

--- task5.js
+++
@@ -1,9 +0,0 @@
-var age = 45;
-
-var ageGroup =
-    (age >= 0 && age <= 12) ? "Child" :
-    (age >= 13 && age <= 19) ? "Teen" :
-    (age >= 20 && age <= 64) ? "Adult" :
-    "Senior";
-
-console.log("Age Group:", ageGroup);
