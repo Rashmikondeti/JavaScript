97d77f5ebf076fefc9bc1f9214a3af53a8852f10
Kondeti Rashmi <rashmikondeti18@gmail.com>
2026-03-04 16:17:53 +0530

    Delete task2.js

--- task2.js
+++
@@ -1,8 +0,0 @@
-var number = 7;
-var age = 16;
-
-var result = (number % 2 === 0) ? "Even" : "Odd";
-var ageResult = (age >= 18) ? "Adult" : "Minor";
-
-console.log("The number is:", result);
-console.log("Age:", ageResult);
